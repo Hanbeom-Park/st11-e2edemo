76be6453529beb7dcb2cbbd2305d24b14547cb34
Hanbeom-Park <hb1398@naver.com>
2024-02-19 17:18:30 +0900

    first init

---
+++ src/main/java/com/ktds/st11e2edemo/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.ktds.st11e2edemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hi. Welcome to CNA apps on the Cloud - v1";
+    }
+}
---
+++ src/main/java/com/ktds/st11e2edemo/St11E2edemoApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.st11e2edemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class St11E2edemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(St11E2edemoApplication.class, args);
+    }
+
+}
