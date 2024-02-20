3ce21c372b2b7e184d31e4a87f2264ec8839fb1d
Hanbeom-Park <hb1398@naver.com>
2024-02-20 14:43:58 +0900

    Fourth Commit

--- src/main/java/com/ktds/st11e2edemo/HelloWorldController.java
+++ src/main/java/com/ktds/st11e2edemo/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String sayHello() {
+
         return "Hi. Welcome to CNA apps on the Cloud - v3";
     }
 }
